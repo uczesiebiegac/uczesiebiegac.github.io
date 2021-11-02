04342505ef99f08447050bfd200ab484ba4ea9c4
uczesiebiegac <78176585+uczesiebiegac@users.noreply.github.com>
2021-11-02 20:06:25 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = 'browserslist';
+}
+
+module.exports = {
+    mode: mode, 
+    target: target,
+
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 
+                "css-loader", 
+                "postcss-loader",
+                "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+
+    plugins: [new MiniCssExtractPlugin()],
+
+
+    devtool: false,
+    devServer: {
+       static: "./dist",
+       hot: true,
+      },
+};
